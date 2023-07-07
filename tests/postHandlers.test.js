037a8b2e989a3f7d27fdf48fb546b05839bb6448
Hanna <hanna.patel.10@gmail.com>
2023-07-07 13:39:16 -0500

    Fixed variables

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -13,7 +13,7 @@ test('Should return 201 status code', async () => {
             method: 'POST',
             headers: {
             'Content-Type': 'application/json'
-	         },
+          },
             body: JSON.stringify(requestBody)
         });
         actualStatusCode = response.status;
