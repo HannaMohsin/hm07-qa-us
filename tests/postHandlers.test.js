f56df9cfd45e8bed4b68da52bfadf85e6731cc16
Hanna <hanna.patel.10@gmail.com>
2023-07-07 13:37:34 -0500

    Fixed variables

--- tests/postHandlers.test.js
+++ tests/postHandlers.test.js
@@ -2,39 +2,40 @@
 const config = require('../config');
 
 const requestBody = {
-		"name": "Picnic",
-		"cardId": 7
-	  }// put your body here
+        "name": "Picnic",
+        "cardId": 7
+      }// put your body here
 
 test('Should return 201 status code', async () => {
   let actualStatusCode;
     try {
-		const response = await fetch(`${config.API_URL}/api/v1/kits`, {
-			method: 'POST',
-			headers: {
-			'Content-Type': 'application/json'
-			},
-			body: JSON.stringify(requestBody)
-		});
-		actualStatusCode = response.status;
-	} catch (error) {
-		console.error(error);
-	}
+        const response = await fetch(`${config.API_URL}/api/v1/kits`, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json'
+	         },
+            body: JSON.stringify(requestBody)
+        });
+        actualStatusCode = response.status;
+    } catch (error) {
+       console.error(error);
+    }
+    expect(actualStatusCode).toBe(200);
 });
 
 test('Response should contain Picnic', async () => {
-	let actualResponseBody;
-	  try {
-		  const response = await fetch(`${config.API_URL}/api/v1/kits`, {
-			  method: 'POST',
-			  headers: {
-			  'Content-Type': 'application/json'
-			  },
-			  body: JSON.stringify(requestBody)
-		  });
-		  actualResponseBody = await response.json();
-	  } catch (error) {
-		  console.error(error);
-	  }
-	  expect(actualResponseBody.name).toBe("Picnic");
+    let actualResponseBody;
+     try {
+          const response = await fetch(`${config.API_URL}/api/v1/kits`, {
+              method: 'POST',
+              headers: {
+              'Content-Type': 'application/json'
+               },
+               body: JSON.stringify(requestBody)
+          });
+          actualResponseBody = await response.json();
+        } catch (error) {
+          console.error(error);
+        }
+       expect(actualResponseBody.name).toBe("Picnic");
   });
